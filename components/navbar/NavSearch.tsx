91a1035ba1a1273603b428759800ff39c1fc4ef9
Nick Rabone <n.rabone72@aol.com>
2024-07-04 23:16:06 +0100

    Added Shadcn theme colour

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -2,7 +2,7 @@ import { Input } from "../ui/input"
 
 function NavSearch() {
     return (
-        <Input type='text' />
+        <Input type='text' placeholder='Find a property...' className='max-w-xs dark:bg-muted' />
     )
 }
 export default NavSearch
