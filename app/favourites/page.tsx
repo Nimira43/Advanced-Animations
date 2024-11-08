aaa3853f5141bd365843b2657ab536ff6ea9ccf3
Nimira43 <n.rabone72@aol.com>
2024-11-08 20:28:24 +0000

    Project reboot - created new template

--- app/favourites/page.tsx
+++
@@ -1,7 +0,0 @@
-function FavouritesPage() {
-  return (
-    <h1 className='text-3xl'>FavouritesPage</h1>
-  )  
-}
-export default FavouritesPage
-
--- components/navbar/NavSearch.tsx
+++
@@ -1,8 +0,0 @@
-import { Input } from "../ui/input"
-
-function NavSearch() {
-    return (
-        <Input type='text' placeholder='Find a property...' className='max-w-xs dark:bg-muted' />
-    )
-}
-export default NavSearch
