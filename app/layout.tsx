15622d9766f34b9ad1356d3e1e5c30a716ed0cad
Nimira43 <n.rabone72@aol.com>
2024-11-08 23:54:37 +0000

    Added Providers.tsx and brought into main layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import Navbar from '@/components/navbar/Navbar'
 import { Poppins } from 'next/font/google' 
-import type { Metadata } from 'next';
+import type { Metadata } from 'next'
 import './globals.css'
+import Providers from './providers'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={poppins.className}>
-      <Navbar />
-      <main className='container py-10'>{children}</main>
+        <Providers>
+          <Navbar />
+          <main className='container py-10'>{children}</main>
+        </Providers>
       </body>
     </html>
   )
