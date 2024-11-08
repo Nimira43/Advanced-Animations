47311cf560f51cd390fa368a24def86b6f6d63b4
Nimira43 <n.rabone72@aol.com>
2024-11-08 22:19:29 +0000

    Navbar set up

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from '@/components/ui/button'
 function HomePage() {
   return (
     <>
-      <h1 className='text-3xl'>Siesta Tours</h1>
-      <Button variant='outline' size='lg' className='capitalize m-8'>Submit</Button>
+      <h1>Home Page</h1>
     </>
   )
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
+import DarkMode from './DarkMode'
+import LinksDropdown from './LinksDropdown'
+import Logo from './Logo'
+import NavSearch from './NavSearch'
+
 function Navbar() {
   return (
-    <h1>Navbar</h1>
+    <nav className='borber-b'>
+      <div className='container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
+        <Logo />
+        <NavSearch />
+        <div className='flex gap-4 items-center'>
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
   )
 }
 export default Navbar
