a2c39c017417345c67536ecd30927631cb20660c
Nimira43 <n.rabone72@aol.com>
2024-11-09 00:25:10 +0000

    Dark mode set up and working web

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -1,6 +1,33 @@
-function DarkMode() {
+'use client'
+
+import * as React from 'react'
+import { useTheme } from 'next-themes'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import { Button } from '../ui/button'
+import { PiSunBold, PiMoonBold } from 'react-icons/pi'
+
+export default function ModeToggle() {
+  const { setTheme } = useTheme()
+  
   return (
-    <h1>DarkMode</h1>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' size='icon'>
+          <PiSunBold className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <PiMoonBold className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
-export default DarkMode
