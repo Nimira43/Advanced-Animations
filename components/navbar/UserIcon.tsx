e252d8ceb0481519fb09238982772e49bc55a8f6
Nimira43 <n.rabone72@aol.com>
2024-11-09 02:01:43 +0000

    Few changes to UserIcon

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -2,7 +2,7 @@ import { RiUser6Line } from 'react-icons/ri'
 
 function UserIcon() {
   return (
-    <RiUser6Line className='w-6 h-6 bg-[#87ceeb] rounded-full text-[#111]' />  
+    <RiUser6Line className='w-6 h-6  rounded-full text-[#111] dark:text-[#fff]' />  
   )
 }
 export default UserIcon
