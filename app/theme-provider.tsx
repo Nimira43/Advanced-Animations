39f165d7e509efe5061eafa6a4d724d24f47a82a
Nimira43 <n.rabone72@aol.com>
2024-11-09 00:02:35 +0000

    Theme-providers.tsx done for now.

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -1,5 +1,9 @@
-import * 
+'use client'
 
-export function ThemeProvider({ children, ...props}: ThemeProviderProps) {
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider} from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
