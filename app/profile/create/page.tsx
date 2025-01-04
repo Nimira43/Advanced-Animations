228b0eb29c9c5ab5eebd36f7ba325c29839bd188
Nimira43 <n.rabone72@aol.com>
2025-01-04 12:53:53 +0000

    FormInput component setup done.

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,4 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 import { Button } from "@/components/ui/button"
 
 const createProfileAction = async (formData: FormData) => {
@@ -14,10 +13,7 @@ function CreateProfile() {
       <h1 className='2xl font-semibold mb-8 capitalize'>new user</h1>
       <div className="border p-8 rounded sm max-w-lg">
         <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor='firstName'>First Name</Label>
-            <Input id="firstName" name="firstName" type='type' />
-          </div>
+          
           <Button type='submit' size='lg'>
             Create Profile
           </Button>
--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -1,7 +1,35 @@
- function FormInput() {
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+ 
+type FormInputProps = {
+  name: string
+  type: string
+  label?: string
+  defaultValue?: string
+  placeholder?: string 
+}
+
+function FormInput(props: FormInputProps) {
+  const {label, name, type, defaultValue, placeholder} = props
+
   return (
-    <div>FormInput</div>
+    <div className="mb-2">
+      <Label 
+        htmlFor={name} 
+        className='capitalize'
+      >
+        {label || name}
+      </Label>
+      <Input 
+        id={name} 
+        name={name} 
+        type={type} 
+        defaultValue={defaultValue} 
+        placeholder={placeholder}
+        required
+      />
+    </div>
   )
 }
 
-export default FormInput()
+export default FormInput
