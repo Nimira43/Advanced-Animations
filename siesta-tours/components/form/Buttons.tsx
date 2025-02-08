1037b92ae7cdebe212c3e9115109cdc9e770c03b
Nimira43 <n.rabone72@aol.com>
2025-02-08 13:56:15 +0000

    Reset components inside of form sub folder.

--- siesta-tours/components/form/Buttons.tsx
+++ siesta-tours/components/form/Buttons.tsx
@@ -1,33 +1,40 @@
-'use client'
-import { TfiReload } from 'react-icons/tfi' 
-import { useFormStatus } from 'react-dom'
-import { Button } from '@/components/ui/button'
-
-type SubmitButtonProps = {
-  className?: string
-  text?: string
-}
-
-export function SubmitButton({
-  className = '',
-  text = 'submit',
-}: SubmitButtonProps) {
-  const { pending } = useFormStatus()
+export default function Buttons() {
   return (
-    <Button
-      type='submit'
-      disabled={pending}
-      className={`capitalize ${className}`}
-      size='lg'
-    >
-      {pending ? (
-        <>
-          <TfiReload className='mr-2 h-4 w-4 animate-spin' />
-          Please wait...
-        </>
-      ) : (
-        text
-      )}
-    </Button>
+    <div>Buttons</div>
   )
 }
+
+
+// 'use client'
+// import { TfiReload } from 'react-icons/tfi' 
+// import { useFormStatus } from 'react-dom'
+// import { Button } from '@/components/ui/button'
+
+// type SubmitButtonProps = {
+//   className?: string
+//   text?: string
+// }
+
+// export function SubmitButton({
+//   className = '',
+//   text = 'submit',
+// }: SubmitButtonProps) {
+//   const { pending } = useFormStatus()
+//   return (
+//     <Button
+//       type='submit'
+//       disabled={pending}
+//       className={`capitalize ${className}`}
+//       size='lg'
+//     >
+//       {pending ? (
+//         <>
+//           <TfiReload className='mr-2 h-4 w-4 animate-spin' />
+//           Please wait...
+//         </>
+//       ) : (
+//         text
+//       )}
+//     </Button>
+//   )
+// }
