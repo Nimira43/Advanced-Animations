c76ff0efef4f55ec9eb7044987f0f1eb098b23e5
Nimira43 <n.rabone72@aol.com>
2025-02-08 14:12:46 +0000

    Setting overall background color to #fffcfa in globals.css. Testing out Tailwind in root page.tsx - all good.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 function HomePage() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className='m-5'>
+      <h1 className='text-main-dark text-4xl text-center font-semibold bg-skyblue logo p-4 rounded-lg'>Siesta Tours</h1>
+    </div>
   )
 }
 export default HomePage
