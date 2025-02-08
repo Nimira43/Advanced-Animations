2eb4bb89c612d51abae8e8cba6023b147d2a99fa
Nimira43 <n.rabone72@aol.com>
2025-02-08 15:42:53 +0000

    Brought back old code in ThemeProvidersProps

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -1,16 +1,9 @@
-export default function ThemePovider() {
-  return (
-    <div>ThemeProvider</div>
-  )
+'use client'
+
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider} from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
-
-// 'use client'
-
-// import * as React from 'react'
-// import { ThemeProvider as NextThemesProvider} from 'next-themes'
-// import { type ThemeProviderProps } from 'next-themes'
-
-// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-// }
