37e87b59ead618ae2dda627506168db13d3bd911
Nimira43 <n.rabone72@aol.com>
2025-02-08 14:51:41 +0000

    In app page.tsx - just returning a h1 tage with Home Page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from '@/components/ui/button'
 function HomePage() {
   return (
     <>
-      <h1 className='text-main-dark text-4xl text-center font-semibold bg-skyblue logo p-4 rounded-lg'>Siesta Tours</h1>
-      <Button className='mt-4'>Login</Button>
+    <h1>Home Page</h1>
     </>
   )
 }
