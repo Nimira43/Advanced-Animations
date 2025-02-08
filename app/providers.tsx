e669393ec86102d94078e548aa236b5a4eb04a75
Nimira43 <n.rabone72@aol.com>
2025-02-08 15:37:44 +0000

    In providers.tsx brought back old code - comment out lines not in use yet.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,33 +1,22 @@
-function Providers() {
-  return (
-    <div>
-      <h1>Providers</h1>      
-    </div>
-  )
-}
-
-export default Providers
-
-
-// 'use client'
+'use client'
 
 // import { ThemeProvider } from './theme-provider'
 // import { Toaster } from '@/components/ui/toaster'
 
-// function Providers({ children }: { children: React.ReactNode}) {
-//   return (
-//     <>
-//       <Toaster />
-//       <ThemeProvider
-//         attribute='class'
-//         defaultTheme='system'
-//         enableSystem
-//         disableTransitionOnChange
-//       >
-//         {children}
-//       </ThemeProvider>
-//     </>
+function Providers({ children }: { children: React.ReactNode}) {
+  return (
+    <>
+      {/* <Toaster /> */}
+      {/* <ThemeProvider */}
+        {/* attribute='class' */}
+        {/* defaultTheme='system' */}
+        {/* enableSystem */}
+        {/* disableTransitionOnChange */}
+      >
+        {children}
+      {/* </ThemeProvider> */}
+    </>
     
-//   )
-// } 
-// export default Providers
+  )
+} 
+export default Providers
