7b3f0b1484d3d99ecd4b350b9aec62a438c7848e
Nimira43 <n.rabone72@aol.com>
2025-02-08 14:39:26 +0000

    Added suppressHydrationWarning to HTML tage in layout.tsx. Brought in Navbar component in and created a main tag with the .container class and wrapped children prop inside. Commented out old code not needed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Poppins } from 'next/font/google'
+import Navbar from '@/components/navbar/Navbar'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,47 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={poppins.className}>
-
-        {children}
+        <Navbar />
+        <main className='container py-10'>
+          {children}
+        </main>
       </body>
     </html>
   )
 }
 
-// import Navbar from '@/components/navbar/Navbar'
-// import { ClerkProvider } from '@clerk/nextjs'
-// import { Poppins } from 'next/font/google' 
-// import type { Metadata } from 'next'
-// import './globals.css'
-// import Providers from './providers'
-
-// const poppins = Poppins({
-//   subsets: ['latin'],
-//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-// })
-
-// export const metadata: Metadata = {
-//   title: 'Siesta Tours',
-//   description: 'Made with Next JS, TypeScript, Tailwind, Shadcn, Clerk, Zod, Prisma and Supabase',
-// }
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <ClerkProvider>
-//       <html lang='en' suppressHydrationWarning>
-//         <body className={poppins.className}>
-//           <Providers>
-//             <Navbar />
-//             <main className='container py-10'>{children}</main>
-//           </Providers>
-//         </body>
-//       </html>
-//     </ClerkProvider>   
-//   )
-// }
