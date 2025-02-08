43c02da6f8a8100541f0abc8f9260cfc79151341
Nimira43 <n.rabone72@aol.com>
2025-02-08 14:41:05 +0000

    Installed react icons.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Button } from '@/components/ui/button'
 
 function HomePage() {
   return (
-    <div className='m-5'>
+    <>
       <h1 className='text-main-dark text-4xl text-center font-semibold bg-skyblue logo p-4 rounded-lg'>Siesta Tours</h1>
       <Button className='mt-4'>Login</Button>
-    </div>
+    </>
   )
 }
 export default HomePage
